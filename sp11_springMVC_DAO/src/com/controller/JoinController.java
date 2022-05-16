7d09e91349add114b18dffa81d7895b9f55f3862
sjoongh <az45687@naver.com>
2022-05-16 15:23:54 +0900

    login

---
+++ sp11_springMVC_DAO/src/com/controller/JoinController.java
@@ -0,0 +1,14 @@
+//package com.controller;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//public class JoinController {
+//	
+//	@Autowired
+//	private MemberDAO memberDAO;
+//	
+//	@RequestMapping(value="/join.do", m)
+//}
--- sp11_springMVC_DAO/src/com/controller/LoginController.java
+++ sp11_springMVC_DAO/src/com/controller/LoginController.java
@@ -21,9 +21,10 @@ public class LoginController {
 		return "loginForm"; // WEB-INF/views/ + loginForm + .jsp
 	}
 	
-	@RequestMapping(value="/login.do", method=RequestMethod.POST)
+	@RequestMapping(value="/join.do", method=RequestMethod.POST)
 	// id와 pwd 값을 필수로 받아와야함 --> required=true
-	public String submit( @RequestParam(value="id", required=true) String id,
+	public String submit( 
+				@RequestParam(value="id", required=true) String id,
 				@RequestParam(value="pwd", required=true) String pwd,
 				HttpSession session // session 객체를 얻기 
 				) throws Exception
