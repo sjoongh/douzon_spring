04363d321660392388f20deefad6dcaf036d1dbd
sjoongh <az45687@naver.com>
2022-05-18 15:40:47 +0900

    sp41

--- sp13_web_mybatis/src/views/MainEntry.java
+++ sp13_web_mybatis/src/views/MainEntry.java
@@ -9,22 +9,22 @@ public class MainEntry {
 
 	public static void main(String[] args) {
 		
-		BoardDAO dao = new BoardDAO();
-		
-		BoardVO vo = new BoardVO();
-//		vo.setTitle("myBatis title");
-//		vo.setWriter("김연아");
-//		vo.setContent("myBatis content.....");
+//		BoardDAO dao = new BoardDAO();
 //		
-//		dao.insertBoard(vo);
-		
-		vo.setSearchCondition("TITLE");
-		vo.setSearchKeyword("22");
-		List<BoardVO> boardList = dao.getBoardList(vo);
-		
-		for (BoardVO board : boardList) {
-			System.out.println("--->" + board.toString());
-		}
+//		BoardVO vo = new BoardVO();
+////		vo.setTitle("myBatis title");
+////		vo.setWriter("김연아");
+////		vo.setContent("myBatis content.....");
+////		
+////		dao.insertBoard(vo);
+//		
+//		vo.setSearchCondition("TITLE");
+//		vo.setSearchKeyword("22");
+//		List<BoardVO> boardList = dao.getBoardList(vo);
+//		
+//		for (BoardVO board : boardList) {
+//			System.out.println("--->" + board.toString());
+//		}
 		
 	}
 
