86f71b5f57d2719eb7603342d24f15eab01b8f8d
sjoongh <az45687@naver.com>
2022-05-24 10:43:17 +0900

    rest api put

---
+++ put/src/main/java/com/example/put/Post1Application.java
@@ -0,0 +1,13 @@
+package com.example.put;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Post1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Post1Application.class, args);
+	}
+
+}
---
+++ put/src/test/java/com/example/post/Post1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Post1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
